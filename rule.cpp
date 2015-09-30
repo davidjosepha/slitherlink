0ed0c0a9ffd434ea733001cfbac6cc01a8060451
lambertv <lambertv@carleton.edu>
2015-09-30 10:55:03 -0500

    Finish rule constructor

--- rule.cpp
+++ rule.cpp
@@ -2,5 +2,8 @@
 #include "enums.h"
 
 Rule::Rule(Lattice const & before, Lattice const & after) {
-    
+    m_ = before.getHeight();
+    n_ = before.getWidth();
+    before_ = &before;
+    after_ = &after;
 }
