a566c8e42ea9dbc4ee8cd9d547ed396cee3354ae
lambertv <lambertv@carleton.edu>
2015-10-01 14:01:10 -0500

    Implement solver's applyRules over entire board

--- solver.cpp
+++ solver.cpp
@@ -5,10 +5,20 @@
 
 Solver::Solver(Lattice & grid) {
     grid_ = &grid;
+    initRules();
+    applyRules();
 }
 
 void Solver::applyRules() {
-
+    for  (int x = 0; x < NUM_RULES; x++) {
+        for (int i = 0; i <= grid_->getWidth() - rules_[x].getWidth(); i++) {
+            for (int j = 0; j <= grid_->getHeight() - rules_[x].getHeight(); j++) {
+                if (ruleApplies(i, j, rules_[x])) {
+                    applyRule(i,j,rules_[x]);
+                }
+            }
+        }
+    }
 }
 
 void Solver::applyRule(int i, int j, Rule & rule) {
@@ -37,4 +47,60 @@ void Solver::applyRule(int i, int j, Rule & rule) {
             }
         }
     }
+}
+
+bool Solver::ruleApplies(int i, int j, Rule & rule) {
+    Lattice const * before = rule.getBefore();
+
+    for (int k = 0; k < rule.getHeight(); k++) {
+        for (int l = 0; l < rule.getWidth(); l++) {
+            if (before->getNumber(k, l) != NONE and
+                before->getNumber(k, l) != grid_->getNumber(k + i, l + j)) {
+                return false;
+            }
+        }
+    }
+
+    for (int k = 0; k < rule.getHeight() + 1; k++) {
+        for (int l = 0; l < rule.getWidth(); l++) {
+            if (before->getHLine(k, l) != EMPTY and
+                before->getHLine(k, l) != grid_->getHLine(k + i, l + j)) {
+                return false;
+            }
+        }
+    }
+
+    for (int k = 0; k < rule.getHeight(); k++) {
+        for (int l = 0; l < rule.getWidth() + 1; l++) {
+            if (before->getVLine(k, l) != EMPTY and
+                before->getVLine(k, l) != grid_->getVLine(k + before->getWidth(), l + before->getHeight())) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+void Solver::initRules() {
+
+    int i = 0;
+
+    /* Rule #1 */
+    beforeLattices_[i].initArrays(2, 2);
+    beforeLattices_[i].cleanArrays();
+
+    beforeLattices_[i].setNumber(1, 0, THREE);
+    beforeLattices_[i].setNumber(0, 1, THREE);
+
+    afterLattices_[i].initArrays(2, 2);
+    afterLattices_[i].cleanArrays();
+
+    afterLattices_[i].setHLine(0, 1, LINE);
+    afterLattices_[i].setHLine(2, 0, LINE);
+    afterLattices_[i].setVLine(1, 0, LINE);
+    afterLattices_[i].setVLine(0, 2, LINE);
+
+    rules_[i] = Rule(beforeLattices_[i], afterLattices_[i]);
+    i++;
 }
--- solver.h
+++ solver.h
@@ -3,14 +3,21 @@
 #include "lattice.h"
 #include "rule.h"
 
+#define NUM_RULES 1
+
 class Solver {
     public:
         Solver(Lattice & grid);
 
     private:
+        void initRules();
         void applyRules();
         void applyRule(int i, int j, Rule & rule);
+        bool ruleApplies(int i, int j, Rule & rule);
         Lattice * grid_;
+        Rule rules_[NUM_RULES];
+        Lattice beforeLattices_[NUM_RULES];
+        Lattice afterLattices_[NUM_RULES];
 };
 
 #endif
