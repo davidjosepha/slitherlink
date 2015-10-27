83e1859f7ef3e9fa32785f3ed5e90e6bf34aff81
David Anderson <davidjosepha@gmail.com>
2015-10-27 14:37:14 -0500

    Add/change comments before methods in Contour class

--- contour.cpp
+++ contour.cpp
@@ -1,5 +1,7 @@
 #include "contour.h"
 
+/* Initialize a contour with end points
+ * (starti, startj) and (endi, endj) */
 Contour::Contour(int starti, int startj, int endi, int endj) {
     start_[0] = starti;
     start_[1] = startj;
@@ -7,6 +9,8 @@ Contour::Contour(int starti, int startj, int endi, int endj) {
     end_[1] = endj;
 }
 
+/* Checks whether the contour instance passed as
+ * input shares an end point with this contour. */
 bool Contour::sharesEndpoint(Contour & contour) {
     return ((start_[0] == contour.start_[0] && start_[1] == contour.start_[1]) ||
             (start_[0] == contour.end_[0] && start_[1] == contour.end_[1]) ||
@@ -14,8 +18,10 @@ bool Contour::sharesEndpoint(Contour & contour) {
             (end_[0] == contour.end_[0] && end_[1] == contour.end_[1]));
 }
 
-// Add another contour as part of this contour. If it doesn't share any endpoints nothing happens
-// sets closed_ to be true if new contour closes the contour
+/* Add another contour as part of this contour. If
+ * it doesn't share any endpoints, nothing happens.
+ * Sets closed_ to true if new contour closes the
+ * contour. */
 void Contour::addContour(Contour & contour) {
     if (start_[0] == contour.start_[0] && start_[1] == contour.start_[1]) {
         start_[0] = contour.end_[0];
