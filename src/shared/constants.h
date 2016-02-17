9a1538ad626c35cb1540d21ee3c572000eee99b2
edwardkw <erkdrummer@gmail.com>
2016-02-17 16:47:37 -0600

    adding basic 'rules' constant for all rules

--- src/shared/constants.h
+++ src/shared/constants.h
@@ -13,8 +13,10 @@
 #define NUM_BASIC_RULES 5
 #define NUM_GOOD_RULES 21
 
-#define BASIC_RULES {5, 2, 4, 3, 21}
+#define BASIC_RULES {4, 1, 3, 2, 20}
 
-#define GOOD_RULES {5, 2, 4, 3, 21, 24, 1, 11, 10, 20, 12, 27, 9, 17, 8, 7, 28, 14, 6, 13, 19}
+#define GOOD_RULES {4, 1, 3, 2, 20, 23, 0, 10, 9, 19, 11, 26, 8, 16, 7, 6, 27, 13, 5, 12, 18}
+
+#define RULES {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}
 
 #endif
