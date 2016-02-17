4bfbb42fec4f9b67ff571ac4464e02c6bfbc7230
edwardkw <erkdrummer@gmail.com>
2016-02-17 16:23:13 -0600

    adding constants for ruleset customization

--- src/shared/constants.h
+++ src/shared/constants.h
@@ -10,5 +10,11 @@
 #define NUM_RULES 33
 #define NUM_CONST_RULES 3
 #define NUM_CONTRADICTIONS 11
+#define NUM_BASIC_RULES 5
+#define NUM_GOOD_RULES 21
+
+//int BASIC_RULES[5] = {5, 2, 4, 3, 21};
+
+//int GOOD_RULES[21] = {5, 2, 4, 3, 21, 24, 1, 11, 10, 20, 12, 27, 9, 17, 8, 7, 28, 14, 6, 13, 19};
 
 #endif
