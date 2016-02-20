1a010f874d45e94f085d3bd7f54019c6ba3db4c6
David Anderson <davidjosepha@gmail.com>
2016-02-20 17:27:50 -0600

    Take difficulty parameter as argument

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -10,15 +10,20 @@
 int main(int argc, char * argv[]) {
     clock_t startTime, endTime;
     
-    if (argc == 3) {
+    if (argc == 4) {
         std::istringstream mn(argv[1]);
         std::istringstream nn(argv[2]);
+        std::istringstream difficn(argv[3]);
         int m, n;
+        std::string difficstr;
         assert(mn >> m && mn.eof());
         assert(nn >> n && nn.eof());
-        
+        assert(difficn >> difficstr && difficn.eof());
+
+        Difficulty diffic = (difficstr == "e") ? EASY : HARD;
+
         startTime = clock();
-        Generator g = Generator(m, n, EASY); //one square for left/right top/bottom boundries
+        Generator g = Generator(m, n, diffic); //one square for left/right top/bottom boundries
 
         endTime = clock();
         float diff = ((float)endTime - (float)startTime) / CLOCKS_PER_SEC;
